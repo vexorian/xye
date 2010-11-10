caee06f166dd529f1f817f0f5a9b2f1c59fa8520
vexorian <vexorian@gmail.com>
2010-11-10 18:16:41 +0000

    * options.cpp/h : switch to namespace  instead of class full of static members. * all : Add skins.cpp/h , the beginnings of a theme picker.
    git-svn-id: https://svn.code.sf.net/p/xye/code@140 dbc23497-bb88-4a8f-aa66-d0e9199f70ee

--- src/options.h
+++ src/options.h
@@ -1,93 +1,75 @@
 #include "tinyxml/xye_tinyxml.h"
 #include "vxsdl.h"
 
+#ifndef OPTIONS_H
+#define OPTIONS_H 1
+
 using std::string;
 
 const int XYE_WALL_VARIATIONS = 6; 
 
-class options
-{
- private:
-    static bool bini;
-
-
-//    static TiXmlDocument* cnf;
-//    static TiXmlElement* ele;
-    static string LevelFile;
-    static char* Texture;
-    static char* LuminosityTexture;
-    static int   FontSize;
-    static int   FontBoldSize;
-    static char* Font;
-    static char* FontBold;
-    static bool  enundo;
-
-
-    static int GridSize;
-    static unsigned int lvnum;
-    static bool Error(const char* msg);
-    static unsigned char r,g,b;
-    static void Default();
-    static void LoadColors(TiXmlElement* skn);
-    static TiXmlElement* GetOptionsElement(TiXmlDocument* cnf);
-    static TiXmlDocument* getxyeconf(TiXmlElement *&options  );
-    static TiXmlDocument* defaultxyeconf(const char* path,TiXmlElement *&options);
-    static string homefolder;
-    static void LoadLevelFile();
-    static void PerformLevelFileSave();
-
- public:
-    static void IgnoreLevelSave();
-    static bool  xyeDirectionSprites;
-    static string Dir;
-    options() {}
-    static string GetDir();
-    static const string& GetHomeFolder();
-    static string* fixpath(string& path,bool dohomecheck);
-    static string* fixpath(string& path) { return fixpath(path,false); }
-    static char* fixpath(const char * path,bool dohomecheck);
-    static char* fixpath(const char * path) { return fixpath(path,false); }
-    static void Init();
-    static void Clean();
-    static const char* GetLevelFile();
-    static unsigned int GetLevelNumber( const char* levelfile);
+namespace options {
+    
+    struct SkinInformation
+    {
+        int    pw,ph;
+        string title;
+        string author;
+        string description;
+        SDL_Surface* preview;
+    };
+    
+    void IgnoreLevelSave();
+    string GetDir();
+    const string& GetHomeFolder();
+    string  fixpath(const string path, bool dohomecheck=false);
+    char* fixpath(const char * path,bool dohomecheck=false);
+    void Init();
+    void Clean();
+    const char* GetLevelFile();
+    unsigned int GetLevelNumber( const char* levelfile);
     
     
-    static const char* GetSpriteFile();
-    static const char* GetLuminositySpriteFile();
-
-    static int GetFontSize();
-    static int GetFontBoldSize();
-    static const char* GetFontFile();
-    static const char* GetFontBoldFile();
-    static int GetGridSize();
+    const char* GetSpriteFile();
+    const char* GetLuminositySpriteFile();
+    
+    int GetFontSize();
+    int GetFontBoldSize();
+    const char* GetFontFile();
+    const char* GetFontBoldFile();
+    int GetGridSize();
     
     
-    static void SaveLevelFile(const char* filename, int levelNumber = 0);
+    void SaveLevelFile(const char* filename, int levelNumber = 0);
     
     
-    static unsigned char Red();
-    static unsigned char Green();
-    static unsigned char Blue();
-
-    inline static bool UndoEnabled() {return enundo; }
-
-    static SDL_Color OneWayDoorColor;
-    static SDL_Color WallColor      [XYE_WALL_VARIATIONS];
-    static SDL_Color WallSpriteColor[XYE_WALL_VARIATIONS];
+    unsigned char Red();
+    unsigned char Green();
+    unsigned char Blue();
     
-    static SDL_Color BFColor[4];
-    static SDL_Color BKColor[4];
+    bool UndoEnabled();
 
-    static SDL_Color LevelMenu_info;
-    static SDL_Color LevelMenu_selected;
-    static SDL_Color LevelMenu_selectederror;
-    static SDL_Color LevelMenu_menu;
-    static SDL_Color LevelMenu_menutext;
-    static SDL_Color LevelMenu_selectedtext;   
-    static SDL_Color LevelMenu_infotext;
-
-    static string ExecutablePath;
+    extern bool  xyeDirectionSprites;
+    extern string Dir;
+   
+    extern SDL_Color OneWayDoorColor;
+    extern SDL_Color WallColor      [XYE_WALL_VARIATIONS];
+    extern SDL_Color WallSpriteColor[XYE_WALL_VARIATIONS];
     
+    extern SDL_Color BFColor[4];
+    extern SDL_Color BKColor[4];
+    
+    extern SDL_Color LevelMenu_info;
+    extern SDL_Color LevelMenu_selected;
+    extern SDL_Color LevelMenu_selectederror;
+    extern SDL_Color LevelMenu_menu;
+    extern SDL_Color LevelMenu_menutext;
+    extern SDL_Color LevelMenu_selectedtext;   
+    extern SDL_Color LevelMenu_infotext;
+    
+    extern string ExecutablePath;
+    
+    bool GetSkinInformation(const char* file, SkinInformation & si); 
+}
 
-};
+#endif
