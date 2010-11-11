d45c63f0a94fe637012dc74746880d4d712e4e4a
vexorian <vexorian@gmail.com>
2010-11-11 16:25:47 +0000

    Old function repeated in three files RemovePath was renamed to StripPath and to return and take std::string for better code reuse and safety. Moved to gen.cpp/h
    git-svn-id: https://svn.code.sf.net/p/xye/code@148 dbc23497-bb88-4a8f-aa66-d0e9199f70ee

--- src/gen.h
+++ src/gen.h
@@ -6,21 +6,17 @@ using std::string;
 
 //Sets random seed
 void Randomize();
-//returns a random integer in range
-
-
 
 
 template<class T>
 T GetRandom(T min, T max);
 
-
-
 template<class T>
 bool doublematch(T a,T b,T c);
 
-
+//returns a random integer in range
 int GetRandomInt(int min, int max);
+
 //returns a bool depending of chance (for example Chance(0.25) has a 25% chance
 //to return true
 bool Chance(float x);
@@ -30,11 +26,10 @@ unsigned int RandomRGB();
 
 unsigned int MixColors(unsigned int A, unsigned int B);
 
-#ifndef vxsdlh
 struct SDL_Color;
-#endif
-
 SDL_Color& MixColors(SDL_Color& A, SDL_Color& B);
 
 bool TryS2I(std::string &str, int& r);
+
 std::string GetFileNameNoExtension( const char* filename );
+std::string StripPath(const string filename);
