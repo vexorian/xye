bcee36feed714fe4df1336fe8907fd2cd5644158
vexorian <vexorian@gmail.com>
2011-10-24 12:44:58 +0000

    The listbox now handles its key events itself. And some other tweaks.
    git-svn-id: https://svn.code.sf.net/p/xye/code@348 dbc23497-bb88-4a8f-aa66-d0e9199f70ee

--- src/listbox.h
+++ src/listbox.h
@@ -29,7 +29,7 @@ class listbox: public control
     virtual void   selectItem(int index)=0;
     virtual void   selectItem(string value)=0;
     
-    
+    virtual void   onKeyUp(SDLKey keysim, Uint16 unicode) = 0;
     
 };
 
