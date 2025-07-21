96526df99f706d7985fd5ab4c8bd782f19d26e9a
vexorian <vexorian@gmail.com>
2025-07-20 23:02:40 -0400

    Renew copyright year, we'd like the protections of a Free Software License to stay here for quite a while.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  License (it is a PNG/ZLIB license)
 
-Copyright (c) 2006 Victor Hugo Soliz Kuncar
+Copyright (c) 2025 Victor Hugo Soliz Kuncar
 
 This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
 
